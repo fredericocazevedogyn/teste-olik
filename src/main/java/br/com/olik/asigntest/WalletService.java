5f4de101b347e7a93ed965515ed7c309ed6a9252
Frederico Carvalho de Almeida Azevedo <fredericocazevedo@gmail.com>
2025-06-08 20:33:10 -0300

    Adequacao da verificação se o usuario pertence ao Itau e o saldo é menor que zero. Criação da classe de Exception Handler para mostrar a mensagem de exceção.

--- src/main/java/br/com/olik/asigntest/WalletService.java
+++ src/main/java/br/com/olik/asigntest/WalletService.java
@@ -2,13 +2,10 @@ package br.com.olik.asigntest;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
 @Service
 @AllArgsConstructor
@@ -25,29 +22,27 @@ public class WalletService {
 
     public BigDecimal transaction(@RequestBody TransactionDto transactionDto) {
         log.info("Start Transaction {}", transactionDto);
+
         Wallet wallet = walletRepository.findByUserId(transactionDto.getUserId());
+        BigDecimal novoSaldo = wallet.getAmount().add(transactionDto.getAmount());
+        wallet.setAmount(novoSaldo);
 
-        wallet.setAmount(wallet.getAmount().add(transactionDto.getAmount()));
+        validarSaldo(wallet);
+        persistirWallet(wallet);
 
-        if(wallet.getType() == Wallet.WalletType.ITAU) {
-            getSaldoItau(wallet);
-        } else {
-            persistirWallet(wallet);
-        }
-
-        log.info("Wallet {}", wallet);
+        log.info("Wallet atualizada: {}", wallet);
         return wallet.getAmount();
     }
 
-    private void getSaldoItau(Wallet wallet) {
-        if(wallet.getAmount().compareTo(BigDecimal.ZERO) < 0) {
-            throw new WalletException("Saldo insuficiente! ");
+    private void validarSaldo(Wallet wallet) {
+        if (wallet.getType() == Wallet.WalletType.ITAU && wallet.getAmount().compareTo(BigDecimal.ZERO) < 0) {
+            throw new WalletException("Saldo insuficiente para realizar a operação.");
         }
-        persistirWallet(wallet);
     }
 
     private void persistirWallet(Wallet wallet) {
         walletRepository.save(wallet);
     }
+
 
 }
